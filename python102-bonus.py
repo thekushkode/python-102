83dbfdcafc4b32d43bc8bb01f02cc801415fb732
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-01 15:38:20 -0400

    adding completed bonus exercise for python-102

---
+++ python102-bonus.py
@@ -0,0 +1,7 @@
+# Python-102 Bonus Exercise
+
+# Asking user for 3 pieces of input but using one variable
+three_info = input('Whats your pets name: '), input('What street did you grow up on: '), input('Enter an adjective: ')
+
+# printing in specified order
+print(f'Hello, the {three_info[2].capitalize()} {three_info[0].capitalize()} {three_info[1].capitalize()}!')
